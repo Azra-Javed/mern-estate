7a1405717b65396ec772b36d9a97f9fb640642b8
Azra Javed <azraj063@gmail.com>
2025-05-29 11:36:46 +0500

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ProfilePage from "./pages/ProfilePage";
+import SignUpPage from "./pages/SignUpPage";
+import SigninPage from "./pages/SigninPage";
+
 function App() {
-  return (
-    <>
-      <h1 className="bg-pink-300 text-white">hello world</h1>
-    </>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage />,
+    },
+
+    {
+      path: "/sign-in",
+      element: <SigninPage />,
+    },
+
+    {
+      path: "/sign-up",
+      element: <SignUpPage />,
+    },
+
+    {
+      path: "/about",
+      element: <AboutPage />,
+    },
+
+    {
+      path: "/profile",
+      element: <ProfilePage />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
