44532f1ef68905d3ba43937e31f6233414e1afc3
Azra Javed <azraj063@gmail.com>
2025-06-02 19:51:26 +0500

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
+
+// timestamp
+// 1 -> record the time of user creation
+/// 2-> record the time of the update -> helpful in the sorting
